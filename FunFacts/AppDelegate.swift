3c81e26ca45d55d96550f795a3023a361519bf48
Samuel Yanez <samuelyanez94@gmail.com>
2017-02-02 11:36:53 -0500

    Initial Commit

---
+++ FunFacts/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  FunFacts
+//
+//  Created by Samuel Yanez on 02/01/17.
+//  Copyright © 2017 Samuel Yanez. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  FunFacts
+//
+//  Created by Samuel Yanez on 02/01/17.
+//  Copyright © 2017 Samuel Yanez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var funFactLabel: UILabel!
+    
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factModel = FactModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        funFactLabel.text = factModel.getRandomFact()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+ 
+    @IBAction func showFunFact() {
+        let randomColor = ColorModel().getRandomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        funFactLabel.text = factModel.getRandomFact()
+    }
+
+}
+
+
+
+
+
+
